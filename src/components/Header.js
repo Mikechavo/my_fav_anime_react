b5f839236dbc3b4c8e2b0e3cec0caf6140e102a9
Mikechavo <mchavez76@yahoo.com>
2025-06-19 14:30:06 -0500

    updated list and header

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,10 +12,40 @@ function Header() {
           <Nav className="me-auto">
             <Nav.Link as={Link} to="/" className="text-light">Home</Nav.Link> {/* Use text-light class for white text */}
             <NavDropdown title={<span className="text-light">My Anime List</span>} id="basic-nav-dropdown">
-              <NavDropdown.Item as={Link} to="/jjk" >Jujutsu Kaisen</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/berserk" >Berserk</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/goblin" >Goblin Slayer</NavDropdown.Item>
-            </NavDropdown>
+
+  {/* Clickable links */}
+  <NavDropdown.Item as={Link} to="/jjk">Jujutsu Kaisen</NavDropdown.Item>
+  <NavDropdown.Item as={Link} to="/berserk">Berserk</NavDropdown.Item>
+  <NavDropdown.Item as={Link} to="/goblin">Goblin Slayer</NavDropdown.Item>
+
+  <NavDropdown.Divider />
+
+  {/* Non-clickable items with line breaks */}
+  <NavDropdown.ItemText className="text-muted" style={{ whiteSpace: 'pre-line' }}>
+    {`Demon Slayer
+Mushoku Tensei Jobless Reincarnation
+Solo Leveling
+My Hero Academia
+Heavenly Delusion
+Kaiju No. 8
+Hell's Paradise
+Ninja Kamui
+Undead Unluck
+Tower of God
+Dandadan
+Chainsaw Man
+Black Clover
+Dorohedoro
+Blue Exorcist
+Castlevania
+The Witch and The Beast
+The Devouring Whale
+To Be Hero X
+Made in Abyss
+Mecha-Ude`}
+  </NavDropdown.ItemText>
+
+</NavDropdown>
             <Nav.Link as={Link} to="/contact" className="text-light">Contact Me</Nav.Link>
           </Nav>
         </Navbar.Collapse>
